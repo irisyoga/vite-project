8650285630a56341b438ca0f86877cd786761a00
Iryna Haber <gaber.ira@gmail.com>
2025-11-03 12:24:20 +0100

    Thema:Light-Dark ProductsPage

--- src/components/ProductsPage/ProductPage.tsx
+++ src/components/ProductsPage/ProductPage.tsx
@@ -1,8 +1,11 @@
 import { useEffect, useState, type JSX } from "react";
 import { Link, useParams } from "react-router-dom";
 import type Product from "./type/Product";
+import { useTheme } from "../ThemeContext/useTheme";
+import style from "../UsersPage/UserPage.module.css";
 
 export default function ProductPage(): JSX.Element {
+  const { theme, toggleTheme } = useTheme(); //Используем хук для темы
   const { productId } = useParams();
   const initialValue: Product = {
     id: 0,
@@ -10,13 +13,13 @@ export default function ProductPage(): JSX.Element {
     price: 0.1,
     description: "string",
     category: "string",
-    image: "http://example.com",
+    image: "string",
   };
 
   const [product, setProduct] = useState<Product>(initialValue);
   useEffect(() => {
     async function loadProduct(): Promise<void> {
-      const res = await fetch(`https://fakestoreapi.com/products/${productId}`);
+      const res = await fetch(`https://fakestoreapi.com/products/{id}`);
       const obj = await res.json();
       setProduct(obj);
     }
@@ -24,6 +27,12 @@ export default function ProductPage(): JSX.Element {
   }, [productId]);
 
   return (
+    <div>
+       <div
+      className={`${style.container} ${
+        theme === "dark" ? style.dark : style.light
+      }`}
+    ></div>
     <div style={{ border: "solid black 2px", margin: "10px" }} key={product.id}>
       <div>Title: {product.title}</div>
       <div>Category: {product.category}</div>
@@ -33,6 +42,14 @@ export default function ProductPage(): JSX.Element {
 
       <Link to="../productsPage">К списку продуктов</Link>
     </div>
+       <button
+          type="button"
+          className={style.themeToggle}
+          onClick={toggleTheme}
+        >
+          Переключить тему на (текущая: {theme})
+        </button>
+
+        </div>
   );
 }
-
